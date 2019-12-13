a23c4e5c3b55427e8dad445cb0018dfa56a9b865
Elle Jullia De Guzman <ellejulliadeguzman@Elles-MacBook-Pro.local>
2019-12-13 15:10:56 -0500

    h4

--- js/index.js
+++ js/index.js
@@ -2,45 +2,52 @@ console.log(`piece of shit`);
 
 const products = [
   {
-    name: `Air Jordan 1 Retro First Class Flight`,
+    name: `Retro First Class Flight`,
     price: 205.00,
     img: `img/first-class-flight.jpg`,
+    brand: `Brand: Air Jordan 1`,
     description: `Instead of picking a specific memory, outfit, or quote from MJ, the “First Class Flight” opts for a white/yellow colorway equipped with boarding-pass style barcodes and embellishments that anyone who’s traveled by air is sure to be familiar with.`,
     category: `jordans`,
   }, {
-    name: `Air Jordan 1 Retro High Black Crimson Tint (GS)`,
+    name: `Retro High Black Crimson Tint (GS)`,
     price: 227.00,
     img: `img/og-gs.jpg`,
+    brand: `Brand: Air Jordan 1`,
     description: `The Air Jordan 1 High OG GS “Crimson Tint” is the grade school kids version of the popular colorway. Released in early 2019, the Air Jordan 1 adds a coral pink “Crimson Tint” hue to the two-tone color blocking seen on classic looks like the “Bred” and “Royal” colorways.`,
     category: `jordans`,
   }, {
-    name: `Air Jordan 1 Retro High OG "Phantom" GS`,
+    name: `Retro High OG "Phantom" GS`,
     price: 266.00,
     img: `img/phantom.jpg`,
+    brand: `Brand: Air Jordan 1`,
     description:`The Air Jordan 1 High GS “Phantom” is the grade school children’s sizing of the 2019 colorway. The unique construction for the iconic silhouette features a crinkled leather base in Sail with light grey Phantom overlays in leather and University Red accents.`,
     category: `jordans`,
   }, {
-    name: `Air Jordan 1 Retro High Pine Green`,
+    name: `Retro High Pine Green`,
     price: 401.00,
     img: `img/pine-green.jpg`,
+    brand: `Brand: Air Jordan 1`,
     description: ` The Air Jordan 1 Retro High OG ‘Pine Green’ showcases a modified take on the silhouette’s classic ‘Black Toe’ design, first introduced to the world in 1985. The leather high-top employs a black and white color scheme with Pine Green accents on the heel, outsole and leather wings that wrap around the collar. Unlike the original ‘Black Toe,’ the traditional white finish on the toe box is colored in with more Pine Green.`,
     category: `jordans`,
   }, {
-    name: `Air Jordan 1 Retro High Rookie Of The Year`,
+    name: `Retro High Rookie Of The Year`,
     price: 448.95,
     img: `img/rookie-of-the-year.jpg`,
+    brand: `Brand: Air Jordan 1`,
     description: `To celebrate, Jordan Brand released the Air Jordan 1 Retro High OG 'Rookie of the Year.' The high top features a white-based leather upper with perforations on the toe box for breathability and Gold Harvest leather overlays on the mudguard, ankle, eyestay, and heel.`,
     category: `jordans`,
   }, {
-    name:`Air Jordan 1 Retro High OG Turbo Green`,
+    name:`Retro High OG Turbo Green`,
     price: 330.95,
     img: `img/turbo-green.jpg`,
+    brand: `Brand: Air Jordan 1`,
     description: `The Air Jordan 1 Retro High OG ‘Turbo Green’ complements the silhouette’s clean lines with a classic two-tone design. A textured white leather base is contrasted by soft suede overlays in Turbo Green. The latter design element is burnished to give off a vintage aesthetic, a quality that’s further enhanced by a midsole finished in off-white Sail.`,
     category: `jordans`,
   },{
     name:`Supreme 20th Anniversary Box Logo Tee White`,
     price: 1224.00,
     img: `img/box-logo.jpg`,
+    brand: `Brand: Supreme`,
     description: `The Supreme 20th Anniversary Box Logo Tee White is a must have for Supreme fanatics! With the
     iconic plain white tee and red box logo embroidered onto the shirt isntead of it being painted on top. The
     embroidery will surely last longer than the painted version, which is worth your money! `,
@@ -49,6 +56,7 @@ const products = [
     name:`Supreme Brooklyn Tee`,
     price: 770,
     img: `img/brooklyn-logo.jpg`,
+    brand: `Brand: Supreme`,
     description: `Now, we give you an official look at the Brooklyn bogo tee straight from the actual BK store.
     The shirt comes in a white base and features the box logo in black/yellow camo pattern. At the back, the phone
     number of the shop is emblazoned.`,
@@ -57,6 +65,7 @@ const products = [
     name:`Supreme Promise`,
     price: 1170.00,
     img: `img/paris-box-logo.jpg`,
+    brand: `Brand: Supreme`,
     description: `The design of the Supreme Paris box logo tee features the iconic Supreme box logo in black
     with white text, while the back reads ‘Bonjour Madame’ above the store’s opening date, 10-3-2016.`,
     category: `shirts`,
@@ -65,6 +74,7 @@ const products = [
     name:`Supreme Doughnut Joe`,
     price: 2100.00,
     img: `img/doughnut-joe.jpg`,
+    brand: `Brand: Supreme`,
     description: `he design of the Supreme Doughnut Joe Box Logo T-Shirt is a one of a kind shirt that gives
     homage to the iconic Joe's Doughnut's doughnut store in the 1970s.`,
     category: `shirts`,
@@ -72,6 +82,7 @@ const products = [
     name:`Supreme Arch SS`,
     price: 160.00,
     img: `img/arch-ss-top.jpg`,
+    brand: `Brand: Supreme`,
     description: `The Supereme Arch SS shirt is one of the shirts Supreme has dropped that doesn't follow it's
     classic colour + logo box design. Sporting an arched Times New Romans font, coming in different colours, it's no surprise that it is completely sold out as soon as it was released.`,
     category: `shirts`,
@@ -79,6 +90,7 @@ const products = [
     name:`Supreme White Banner`,
     price: 91.00,
     img: `img/white-banner.jpg`,
+    brand: `Brand: Supreme`,
     description: `Released as a part of Supreme's Fall/ Winter 2019 Week 1 drop, the Supreme Banner tee sold out
     in a near instant. The tees dropped in nine different colorways and all feature the same graphic image, a
     landscape photograph of Supreme's original store banner. The Supreme Banner Tee released on August 22nd, 2019
@@ -88,6 +100,7 @@ const products = [
     name:`Nas x Supreme`,
     price: 99.00,
     img: `img/nasty-nas.jpg`,
+    brand: `Brand: Supreme`,
     description: `The Nas T-shirts came in various colors but the black option sold out the fastest, clocking in
     at 37 seconds. The brown and heather grey followed at just around the minute mark while the pale blue, lime
     green, and red exited shopping carts just a few seconds after.`,
@@ -96,6 +109,7 @@ const products = [
     name:`Swarovski x Supreme`,
     price: 1889.00,
     img: `img/swarovski.jpg`,
+    brand: `Brand: Supreme`,
     description: `In April of 1994, Supreme New York first opened their doors in downtown Manhattan. April 2019
     marks 25 years since, and in honor of that, Supreme released a special edition of their iconic box logo hooded
     sweatshirt. Featuring a box logo made of Swarovski Crystals.`,
@@ -104,6 +118,7 @@ const products = [
     name:`Supreme x Virgil Abloh`,
     price: 998.00,
     img: `img/virgil-abloh.jpg`,
+    brand: `Brand: Supreme`,
     description: `The box logo does away with the traditional block red background and switches it for red,
     black, and green. The colorway could be a reinterpretation of the pan-African flag, which has the same colors in
     the same order, albeit in horizontal stripes rather than vertical.`,
@@ -112,6 +127,7 @@ const products = [
     name:`Supreme Mandarin Orange`,
     price: 78.00,
     img: `img/supreme-orange.jpg`,
+    brand: `Brand: Supreme`,
     description: `Supreme's Manadarin Orange Tee was one of two tees released in Week 6 that referenced other
     items dropped earlier in the Spring/Summer 19 season. The tee presents a far more financially viable option for
     all those Supreme fans out there. Supreme's Manadarin Orange Tee released on April 4th, 2019 and retailed at $38
@@ -121,6 +137,7 @@ const products = [
     name:`Supreme 20th Anniversary (Red)`,
     price: 1134.00,
     img: `img/supreme-red.jpg`,
+    brand: `Brand: Supreme`,
     description: `One of the many colours the Supreme 20th Anniversary edition shirts. Similar to the classic
     red that Supreme uses, except it's shade is more of a maroon and the box logo still sporting tne infamous box
     logo.`,
@@ -129,6 +146,7 @@ const products = [
     name:`Supreme San Francisco`,
     price: 584.00,
     img: `img/san-francisco.jpeg`,
+    brand: `Brand: Supreme`,
     description: `This shirt was a very big deal in the Supreme community. When James Jebbia decided to open a
     store in San Francisco, he also decided to design a shirt which was unique to San Francisco.`,
     category: `shirts`,
@@ -136,6 +154,7 @@ const products = [
     name:`Supreme Heroines`,
     price: 75.00,
     img: `img/heroines-tee.jpg`,
+    brand: `Brand: Supreme`,
     description: `After releasing two major collaborations in a row for weeks 5 and 6, Supreme kept things
     in-house for their Week 7 release of the FW19 season which featured their second set of fall tees. Released as a
     part of the tee drop was the Supreme Heroines Tee in six colors. The Heroines Tee comes as a result of the
@@ -146,6 +165,7 @@ const products = [
     name:`Supreme Gonz`,
     price: 166.00,
     img: `img/gonz-tee.jpg`,
+    brand: `Brand: Supreme`,
     description: `The Gonz’s affiliation with Supreme goes way back to the stores beginning on Lafayette Street,
     NYC. Whenever overseas on skate trips, Mark would often send postcards to the late Harold Hunter and the rest of
     the crew at “Supream” (his misspelling), featuring his own doodles and musings, some of which would later make
@@ -164,21 +184,31 @@ function getProductAsHtmlString (prod)  {
     <img src="${prod.img}" alt="Product Image" class="${prod.category}">
     <h2>${prod.name}</h2>
     <h3>$${prod.price}</h3>
+    <h4>${prod.brand}</h4>
     <p>${prod.description}</p>
   </article>`;
 }
+
+/*function isMatchingName(product) {
+  // Trim the input of white space, then uppercase it, then check if it's included
+  if (product.name.toUpperCase().includes(this.trim().toUpperCase())) {
+    return true;
+  } else {
+    return false;
+  }
+
+}*/
+
 
 function renderProducts(arr) {
   document.getElementById('products').innerHTML = arr.map(getProductAsHtmlString).join('');
 }
 
-
 renderProducts(products);
 
-function loadProductByName(event) {
+function loadProductByName(products) {
   const nameImSearchingFor = event.target.value; 
   renderProducts(products.filter(isMatchingName, nameImSearchingFor));
 }
-
 
 document.getElementById('products').addEventListener('input', loadProductByName);
